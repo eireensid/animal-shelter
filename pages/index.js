bd6f5068965b4e916c429773856d7bd0f27bbf58
Irina <sidoren-irina@yandex.ru>
2020-11-22 16:30:49 +0300

    donation block

--- pages/index.js
+++ pages/index.js
@@ -455,6 +455,20 @@ export default function Index(title='Home Page') {
           <button>Больше питомцев</button>
         </div>
 
+        <div className={styles.donationBlock}>
+          <h2>Для пожертвований:</h2>
+          <div className={styles.bankCardWrapper}>
+            <div className={styles.bankCard}>
+              <div className={styles.bankCardNumber}>
+                <img src="/img/banking-card-icon.png" alt="банковская карта для пожертвований"/>
+                <span>5469 5500 6217 8147</span>
+              </div>
+              <div className={styles.bankCardBtn}>
+                <button>Скопировать номер</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </MainLayout>
