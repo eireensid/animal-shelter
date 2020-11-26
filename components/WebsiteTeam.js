04270d7ba3a12b650e830a10a2f21c31eea40182
Irina <sidoren-irina@yandex.ru>
2020-11-26 22:37:40 +0300

    developers component add

---
+++ components/WebsiteTeam.js
@@ -0,0 +1,34 @@
+import styles from '~/styles/components/WebsiteTeam.module.scss'
+
+export default function WebsiteTeam() {
+  return <>
+    <div className={styles.developersBlock}>
+      <h2>Выражаем блогодарность всем, кто принимал участие в разработке сайта:</h2>
+      <div className={styles.developersWrapper}>
+        <div className={styles.developer}>
+          <h4>Дизайн:</h4>
+          <img src="/img/developers/website-design.png"/>
+          <span>@yuliach_design</span>
+        </div>
+
+        <div className={styles.developer}>
+          <h4>Программирование:</h4>
+          <img src="/img/developers/website-programming.png"/>
+          <span>@eireensid</span>
+        </div>
+
+        <div className={styles.developer}>
+          <h4>Логотип:</h4>
+          <img src="/img/developers/website-logo.png"/>
+          <span>@lish.design</span>
+        </div>
+
+        <div className={styles.developer}>
+          <h4>Видеосъёмка:</h4>
+          <img src="/img/developers/website-video.png"/>
+          <span>@vita.videomaker</span>
+        </div>
+      </div>
+    </div>
+  </>
+}
