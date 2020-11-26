7083ed72ce20d75ded78dfa49f8c5ae8032c16cd
Irina <sidoren-irina@yandex.ru>
2020-11-26 22:32:17 +0300

    footer styles change

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import WebsiteTeam from '~/components/WebsiteTeam'
 import styles from '~/styles/MainLayout.module.scss'
 
 export function MainLayout({children, title='Animal shelter'}) {
@@ -32,41 +33,47 @@ export function MainLayout({children, title='Animal shelter'}) {
       </main>
       <footer>
         <div className={styles.footerBlock}>
-          <nav className={styles.aboutShelter}>
-            <img className={styles.logoFooter} src="/img/logo.svg" alt="лого"/>
-            <h4>О приюте</h4>
-            <ul>
-              <li><Link href="/"><a>Основные нужды Теремка</a></Link></li>
-              <li><Link href="/"><a>Опекунство и адаптация</a></Link></li>
-              <li><Link href="/"><a>Прогулки с собаками</a></Link></li>
-              <li><Link href="/"><a>Правила посещения приюта</a></Link></li>
-              <li><Link href="/"><a>Приветы из дома</a></Link></li>
-              <li><Link href="/"><a>Отчеты</a></Link></li>
-            </ul>
-          </nav>
-          <nav className={styles.ourPets}>
-            <h4>Наши питомцы</h4>
-            <ul>
-              <li><Link href="/"><a>Кошки</a></Link></li>
-              <li><Link href="/"><a>Собаки</a></Link></li>
-              <li><Link href="/"><a>Ищут дом</a></Link></li>
-              <li><Link href="/"><a>Проходят лечение</a></Link></li>
-              <li><Link href="/"><a>Нужен опекун</a></Link></li>
-              <li><Link href="/"><a>Нужна адаптация</a></Link></li>
-              <li><Link href="/"><a>Малыши</a></Link></li>
-            </ul>
-          </nav>
-          <nav className={styles.contacts}>
-            <h4>Контакты</h4>
-            <p className={styles.phone}><a href="#">+7 (921) 432-00-35</a></p>
-            <p className={styles.address}>г. Санкт-Петербург,<br/>поселок под Гатчиной</p>
-            <h4>Помочь с пиаром</h4>
-            <div className={styles.socialsBlockFooter}>
-              <img src="/img/whatsapp.svg" alt="what's app"/>
-              <img src="/img/vk.svg" alt="vk"/>
-              <img src="/img/instagram.svg" alt="instagram"/>
-            </div>
-          </nav>
+          <img className={styles.logoFooter} src="/img/logo.svg" alt="лого"/>
+          <div className={styles.navBlock}>
+            <nav className={styles.aboutShelter}>
+              <h4>О приюте</h4>
+              <ul>
+                <li><Link href="/"><a>Основные нужды Теремка</a></Link></li>
+                <li><Link href="/"><a>Опекунство и адаптация</a></Link></li>
+                <li><Link href="/"><a>Прогулки с собаками</a></Link></li>
+                <li><Link href="/"><a>Правила посещения приюта</a></Link></li>
+                <li><Link href="/"><a>Приветы из дома</a></Link></li>
+                <li><Link href="/"><a>Отчеты</a></Link></li>
+              </ul>
+            </nav>
+            <nav className={styles.ourPets}>
+              <h4>Наши питомцы</h4>
+              <ul>
+                <li><Link href="/"><a>Кошки</a></Link></li>
+                <li><Link href="/"><a>Собаки</a></Link></li>
+                <li><Link href="/"><a>Ищут дом</a></Link></li>
+                <li><Link href="/"><a>Проходят лечение</a></Link></li>
+                <li><Link href="/"><a>Нужен опекун</a></Link></li>
+                <li><Link href="/"><a>Нужна адаптация</a></Link></li>
+                <li><Link href="/"><a>Малыши</a></Link></li>
+              </ul>
+            </nav>
+            <nav className={styles.contacts}>
+              <h4>Контакты</h4>
+              <p className={styles.phone}><a href="#">+7 (921) 432-00-35</a></p>
+              <p className={styles.address}>г. Санкт-Петербург,<br/>поселок под Гатчиной</p>
+              <h4 className={styles.prHelp}>Помочь с пиаром</h4>
+              <div className={styles.socialsBlockFooter}>
+                <img src="/img/whatsapp.svg" alt="what's app"/>
+                <img src="/img/vk.svg" alt="vk"/>
+                <img src="/img/instagram.svg" alt="instagram"/>
+              </div>
+            </nav>
+          </div>
+
+          <div>
+            <WebsiteTeam/>
+          </div>
         </div>
       </footer>
     </>
