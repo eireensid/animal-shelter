f6d48a720abdf503b418216b19a4a4b52f39b885
Irina <sidoren-irina@yandex.ru>
2020-11-30 23:18:26 +0300

    background wave fix

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,9 @@ export default function Index(title='Главная') {
         <div className={styles.firstBlockImg}>
           <img src="/img/first-block-animals-cut-nobg.png" alt="животные"/>
         </div>
-        <div className={styles.waveBlock}>
+        {/* <div className={styles.waveBlock}>
           <img className={styles.wave} src="/img/yellow-wave.png" />
-        </div>
+        </div> */}
       </div>
     </section>
 
