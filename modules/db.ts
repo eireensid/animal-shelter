15774dce3d49629efb6d873e41b374ae9dbe3dcc
sid-max1996 <sid-max1996@yandex.ru>
2020-12-05 21:56:21 +0300

    pets db

--- modules/db.ts
+++ modules/db.ts
@@ -1,3 +1,4 @@
 const Datastore = require('nedb')
 
 export const usersDb: Nedb = new Datastore({ filename: './databases/users.db', autoload: true })
+export const petsDb: Nedb = new Datastore({ filename: './databases/pets.db', autoload: true })
---
+++ modules/pet.ts
@@ -0,0 +1,55 @@
+import { petsDb } from '~/modules/db'
+
+export type Pet = {
+  name: string,
+  character: string,
+  sex: boolean,
+  date: string,
+  grafting: boolean,
+  sterilization: boolean,
+  description: string
+}
+
+export function getAllPets (): Promise<Array<Pet>> {
+  return new Promise((resolve, reject) => {
+    petsDb.find({}, (err, pets) => {
+      if (err) return reject(err)
+      resolve(pets)
+    })
+  })
+}
+
+export function findPet (obj: any): Promise<Pet> {
+  return new Promise((resolve, reject) => {
+    petsDb.findOne(obj, (err, doc) => {
+      if (err) return reject(err)
+      resolve(doc)
+    })
+  })
+}
+
+export function editPet (pet): Promise<void> {
+  return new Promise(async (resolve, reject) => {
+    petsDb.update({ _id: pet._id }, pet, { multi: true }, (err, doc) => {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}
+
+export function createPet ({
+  name,
+  character,
+  sex,
+  date,
+  grafting,
+  sterilization,
+  description }): Promise<Pet> {
+  return new Promise(async (resolve, reject) => {
+    const pet: Pet = {name, character, sex, date, grafting, sterilization, description}
+    petsDb.insert(pet, (err, doc) => {
+      if (err) return reject(err)
+      resolve(doc)
+    })
+  })
+}
