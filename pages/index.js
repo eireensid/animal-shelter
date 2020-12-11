113c9981219406bd9a9766ae56a6dac65ea62820
Irina <sidoren-irina@yandex.ru>
2020-12-11 21:00:07 +0300

    index style mobile

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Index({title='Главная', animals}) {
       <svg className={styles.waveTablet} viewBox="0 0 768 230" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M552.395 157.261C357.504 133.91 194.806 47.9331 0.0122231 56.5781L-0.0400326 324.797L767.936 324.648L767.973 133.971C691.53 151.895 616.219 164.908 552.395 157.261Z" fill="#FFF5DA"/>
       </svg>
-      <svg className={styles.waveMobile} viewBox="0 0 320 95" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg className={styles.waveMobile} viewBox="0 0 320 110" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M199.367 70.208C128.68 61.7028 65.1238 39.2865 -0.000403353 24.2984L-0.000388884 169.869L320 169.869L320 58.071C277.157 67.8307 235.212 74.5209 199.367 70.208Z" fill="#FFF5DA"/>
       </svg>
     </section>
@@ -84,7 +84,7 @@ export default function Index({title='Главная', animals}) {
               <BankCardModal parent={"index"}/>
             </div>
           </div>
-          <div className={styles.waysToHelpImg}>
+          <div className={`${styles.waysToHelpImg} ${styles.waysToHelpImgMobile}`}>
             <img src="/img/ways-to-help/financial-help.png" alt="финансовая помощь"/>
           </div>
         </div>
