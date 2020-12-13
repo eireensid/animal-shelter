d60cfa3c1ce3499b11ae1dba08f589cd457137f5
Irina <sidoren-irina@yandex.ru>
2020-12-13 17:46:51 +0300

    use dropdown hook, if click outside close

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -1,54 +1,42 @@
 import Link from 'next/link'
-import {useState} from 'react'
+// import {useEffect, useState, useRef} from 'react'
+import useDropdown from '~/hooks/useDropdown'
 import BankCardModal from '~/components/BankCardModal'
 import styles from '~/styles/components/MobileMenu.module.scss'
 
 export default function MobileMenu({closeMenu}) {
-  const [aboutDrodownStyle, setAboutDrodownStyle] = useState({display: "none"})
-  const [petsDropdownStyle, setPetsDropdownStyle] = useState({display: "none"})
-  const openAboutDropdown = (e) => {
-    e.preventDefault()
-    if (aboutDrodownStyle.display === "none") {
-      setAboutDrodownStyle({display: "block"})
-    } else {
-      setAboutDrodownStyle({display: "none"})
-    }
-  }
-  const openPetsDropdown = (e) => {
-    e.preventDefault()
-    if (petsDropdownStyle.display === "none") {
-      setPetsDropdownStyle({display: "block"})
-    } else {
-      setPetsDropdownStyle({display: "none"})
-    }
-  }
+  const { dropdownStyles, openDropdownClicks, wrapperRefs } = useDropdown(2)
 
   return <>
     <div className={styles.mobileMenuBlock}>
       <div className={styles.menuAndBtnBlock}>
         <nav className={styles.menuWrapperMobile}>
-          <a onClick={openAboutDropdown} className={styles.menuAboutLink}>О приюте</a>
-          <div style={aboutDrodownStyle} className={styles.menuDropdownMobile}>
-            <ul className={styles.menuDropdownMobileUl}>
-              <li><Link href="/basic-needs-of-teremok"><a>Основные нужды Теремка</a></Link></li>
-              <li><Link href="/guardianship-and-adaptation"><a>Опекунство и адаптация</a></Link></li>
-              <li><Link href="/walks-with-dogs"><a>Прогулки с собаками</a></Link></li>
-              <li><Link href="/rules-for-shelter-visiting"><a>Правила посещения приюта</a></Link></li>
-              <li><Link href="/hellos-from-homes"><a>Приветы из дома</a></Link></li>
-              <li><Link href="/shelter-reports"><a>Отчеты</a></Link></li>
-            </ul>
+          <div ref={wrapperRefs[0]}>
+            <a onClick={openDropdownClicks[0]} className={styles.menuAboutLink}>О приюте</a>
+            <div style={dropdownStyles[0]} className={styles.menuDropdownMobile}>
+              <ul className={styles.menuDropdownMobileUl}>
+                <li><Link href="/basic-needs-of-teremok"><a>Основные нужды Теремка</a></Link></li>
+                <li><Link href="/guardianship-and-adaptation"><a>Опекунство и адаптация</a></Link></li>
+                <li><Link href="/walks-with-dogs"><a>Прогулки с собаками</a></Link></li>
+                <li><Link href="/rules-for-shelter-visiting"><a>Правила посещения приюта</a></Link></li>
+                <li><Link href="/hellos-from-homes"><a>Приветы из дома</a></Link></li>
+                <li><Link href="/shelter-reports"><a>Отчеты</a></Link></li>
+              </ul>
+            </div>
           </div>
-          <a onClick={openPetsDropdown} className={styles.menuPetsLink}>Питомцы</a>
-          <div style={petsDropdownStyle} className={styles.menuDropdownMobile}>
-            <ul className={styles.menuDropdownMobileUl}>
-              <li><Link href="/cats"><a>Кошки</a></Link></li>
-              <li><Link href="/dogs"><a>Собаки</a></Link></li>
-              <li><Link href="/basic-needs-of-teremok"><a>Ищут дом</a></Link></li>
-              <li><Link href="/undergo-treatment"><a>Проходят лечение</a></Link></li>
-              <li><Link href="/need-guardian"><a>Нужен опекун</a></Link></li>
-              <li><Link href="/need-adoptation"><a>Нужна адаптация</a></Link></li>
-              <li><Link href="/baby-pets"><a>Малыши</a></Link></li>
-            </ul>
+          <div ref={wrapperRefs[1]}>
+            <a onClick={openDropdownClicks[1]} className={styles.menuPetsLink}>Питомцы</a>
+            <div style={dropdownStyles[1]} className={styles.menuDropdownMobile}>
+              <ul className={styles.menuDropdownMobileUl}>
+                <li><Link href="/cats"><a>Кошки</a></Link></li>
+                <li><Link href="/dogs"><a>Собаки</a></Link></li>
+                <li><Link href="/basic-needs-of-teremok"><a>Ищут дом</a></Link></li>
+                <li><Link href="/undergo-treatment"><a>Проходят лечение</a></Link></li>
+                <li><Link href="/need-guardian"><a>Нужен опекун</a></Link></li>
+                <li><Link href="/need-adoptation"><a>Нужна адаптация</a></Link></li>
+                <li><Link href="/baby-pets"><a>Малыши</a></Link></li>
+              </ul>
+            </div>
           </div>
           <Link href="/hellos-from-homes"><a>Приветы из дома</a></Link>
           <Link href="/shelter-reports"><a>Отчеты</a></Link>
---
+++ hooks/useDropdown.js
@@ -0,0 +1,58 @@
+import {useState, useEffect, useRef} from 'react'
+
+export default function useDropdown (count) {
+  const dropdownStyles = []
+  const setDropdownStyles = []
+  const openDropdownClicks = []
+  const wrapperRefs = []
+
+  for (let i = 0; i < count; i++) {
+    const wrapperRef = useRef()
+    wrapperRefs.push(wrapperRef)
+
+    const [dropdownStyle, setDropdownStyle] = useState({display: "none"})
+    dropdownStyles.push(dropdownStyle)
+    setDropdownStyles.push(setDropdownStyle)
+    const index = i
+    const openDropdownClick = (e) => {
+      e.preventDefault()
+      const dropdownStyle = dropdownStyles[index]
+      const setDropdownStyle = setDropdownStyles[index]
+      if (dropdownStyle.display === "none") {
+        setDropdownStyle({display: "block"})
+      } else {
+        setDropdownStyle({display: "none"})
+      }
+      for (let j = 0; j < count; j++) {
+        if (j !== index) {
+          const setDropdownStyle = setDropdownStyles[j]
+          setDropdownStyle({display: "none"})
+        }
+      }
+    }
+    openDropdownClicks.push(openDropdownClick)
+  }
+
+  const handleClick = e => {
+    for (let i = 0; i < count; i++) {
+      const wrapperRef = wrapperRefs[i]
+      if (wrapperRef.current.contains(e.target) || e.target === wrapperRef.current) {
+        return
+      }
+    }
+    for (let i = 0; i < count; i++) {
+      const setDropdownStyle = setDropdownStyles[i]
+      setDropdownStyle({display: "none"})
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick)
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick)
+    }
+  }, [])
+
+  return { dropdownStyles, openDropdownClicks, wrapperRefs }
+}
