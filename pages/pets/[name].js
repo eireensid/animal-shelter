4ea8adf4880690c74e46fd4e9f67925bd77aef33
Irina <sidoren-irina@yandex.ru>
2020-12-16 17:21:47 +0300

    add use pagination hook

--- pages/pets/[name].js
+++ pages/pets/[name].js
@@ -1,8 +1,65 @@
 import {useRouter} from 'next/router'
+import {MainLayout} from '~/components/MainLayout'
+import BreadCrumbs from '~/components/BreadCrumbs'
+import PawLabel from '~/components/PawLabel'
+import AnimalCarousel from '~/components/AnimalCarousel'
+import BankCard from '~/components/BankCard'
+import styles from '~/styles/pet.module.scss'
 
-export default function Pet() {
+export default function Pet({title="pet", animals}) {
   const router = useRouter()
-  return <>
-    <h1>{router.query.name}</h1>
-  </>
+  const filteredAnimals = animals.filter((item) => {
+    return item.paw === "looking-for-home.png"
+  })
+  {/* <h1>{router.query.name}</h1> */}
+  return <MainLayout>
+    <section>
+      <div className={`${styles.petInfoBlock} ${styles.pagePaddings}`}>
+        <div className={styles.breadCrumbsWrapper}>
+          <BreadCrumbs title={title}/>
+        </div>
+        <div>
+          <div>
+            <div className={styles.titleBlock}>
+              <h2>Надюша</h2>
+              <PawLabel pawColor="/img/paws/looking-for-home.png" pawMeaning="Ищут дом"/>
+            </div>
+            <p>
+            Трогательное, мягкое и ласковое существо. Собачка среднего размера с густой шёрсткой и красивыми грустными глазками.<br/>
+            Девочке около 5 лет. Надюша доверчивая и очень нежная, ведёт себя прекрасно, внимательно относится к командам и очень послушна, отлично ходит на поводке. А ещё Надюша — великолепная охранница!<br/>
+            Девочка здоровая, у нее есть опекун, но она мечтает о доме. Пусть вас не обманывают её опущенные ушки, Надюше комфортно и хорошо с человеком
+            </p>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <p>Так же вы можете прийти к питомцу в гости, принести ей вкусняшку или выйти на прогулку. Подробнее о прогулках и посещении приюта можно прочитать здесь:</p>
+          <button>Подробнее</button>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div className={`${styles.animalCarouselWrapper} ${styles.pagePaddings}`}>
+        <AnimalCarousel animals={filteredAnimals} title="Сейчас ищут дом:"/>
+      </div>
+    </section>
+    <section className={styles.bankCardWrapperSection}>
+      <div className={`${styles.bankCardWrapper} ${styles.pagePaddings}`}>
+        <h2>Для пожертвований:</h2>
+        <BankCard/>
+      </div>
+      <svg className={styles.waveDesktop} viewBox="0 0 1440 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M848.411 95.4772C629.058 70.6965 450.479 -28.8145 221.037 8.13269C105.85 26.6814 58.8155 59.5476 0.200227 95.551L0.184691 253.285L1444.17 253.16L1444.19 30.473C1320.11 -28.8791 1035.12 116.57 848.411 95.4772Z" fill="#FFC393"/>
+      </svg>
+      <svg className={styles.waveTablet} viewBox="0 0 768 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M552.395 157.261C357.504 133.91 194.806 47.9331 0.0122231 56.5781L-0.0400326 324.797L767.936 324.648L767.973 133.971C691.53 151.895 616.219 164.908 552.395 157.261Z" fill="#FFC393"/>
+      </svg>
+      <svg className={styles.waveMobile} viewBox="0 0 320 95" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M199.367 70.208C128.68 61.7028 65.1238 39.2865 -0.000403353 24.2984L-0.000388884 169.869L320 169.869L320 58.071C277.157 67.8307 235.212 74.5209 199.367 70.208Z" fill="#FFC393"/>
+      </svg>
+    </section>
+  </MainLayout>
 }
