257a782ea5882c7c5e28391eb3833a3efdab6e21
Irina <sidoren-irina@yandex.ru>
2020-12-16 11:32:07 +0300

    try change paw label and search input styles

--- components/PawLabel.js
+++ components/PawLabel.js
@@ -1,9 +1,26 @@
+import {useState} from 'react'
 import styles from '~/styles/components/PawLabel.module.scss'
 
 export default function PawLabel({pawColor, pawMeaning}) {
+  const [filterStyle, setFilterStyle] = useState({background: "white"})
+  const [imgStyle, setImgStyle] = useState({display: "inline-block"})
+
+  const changeStyle = () => {
+    if (filterStyle.background === "white") {
+      setFilterStyle({background: "red"})
+    } else {
+      setFilterStyle({background: "white"})
+    }
+    if (imgStyle.display === "inline-block") {
+      setImgStyle({display: "none"})
+    } else {
+      setImgStyle({display: "inline-block"})
+    }
+  }
+
   return <>
-    <div className={styles.animalFilter}>
-      <img src={`${pawColor}`}/>
+    <div onClick={changeStyle} style={filterStyle} className={styles.animalFilter}>
+      <img style={imgStyle} src={`${pawColor}`}/>
       <h4>{pawMeaning}</h4>
     </div>
   </>
--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,10 +1,20 @@
+import {useState} from 'react'
 import styles from '~/styles/components/SearchInput.module.scss'
 
 export default function SearchInput() {
+  const [searchInputStyle, setSearchInputStyle] = useState({background: "white", flexDirection: "row"})
+  const changeInputStyle = () => {
+    if (searchInputStyle.background === "white" &&  searchInputStyle.flexDirection === "row") {
+      setSearchInputStyle({background: "red", flexDirection: "row-reverse"})
+    } else {
+      setSearchInputStyle({background: "white", flexDirection: "row"})
+    }
+  }
+
   return <>
-    <div className={styles.searchInput}>
+    <div style={searchInputStyle} className={styles.searchInput}>
       <img src="img/search-icon.png"/>
-      <input type="text" placeholder="Имя питомца"/>
+      <input onClick={changeInputStyle} type="text" placeholder="Имя питомца"/>
     </div>
   </>
 }
