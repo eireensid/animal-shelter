accca3cf451cbb8836a1007dc3ebba3c35de91bc
Irina <sidoren-irina@yandex.ru>
2021-01-07 17:25:23 +0300

    1

--- modules/back/db.ts
+++ modules/back/db.ts
@@ -19,7 +19,7 @@ try {
     storageBucket: process.env.firebase_bucket_url
   })
 } catch (_) {
-  console.error('firebase initializeApp n times serviceAccount1', serviceAccount)
+  console.error('firebase initializeApp n times serviceAccount', serviceAccount)
 }
 
 export default admin
