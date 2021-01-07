7fadcd7b920c6412e100e139ae17ee7416258af9
Irina <sidoren-irina@yandex.ru>
2021-01-07 17:45:16 +0300

    aaa

--- modules/back/db.ts
+++ modules/back/db.ts
@@ -1,10 +1,14 @@
 import admin from 'firebase-admin'
+const replaceAll = require('string.prototype.replaceall')
+
+let fKey = process.env.firebase_private_key
+fKey = replaceAll(fKey, '\\n', '\n')
 
 const serviceAccount: any = {
   type: process.env.firebase_type,
   project_id: process.env.firebase_project_id,
   private_key_id: process.env.firebase_private_key_id,
-  private_key: process.env.firebase_private_key,
+  private_key: fKey,
   client_email: process.env.firebase_client_email,
   client_id: process.env.firebase_client_id,
   auth_uri: process.env.firebase_auth_uri,
