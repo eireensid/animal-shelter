d504c19e4c4fbbcd69939a303f11224878a4da41
Irina <sidoren-irina@yandex.ru>
2021-05-18 20:42:49 +0300

    add meta tag for google

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@700&family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet" />
+          <meta name="google-site-verification" content="b3UAoyiTwEw7EudZmbTujWnN0OkxAw3K2y5Y_uJW1tM" />
         </Head>
         <body>
           <Main />
